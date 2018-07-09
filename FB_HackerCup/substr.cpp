fee7f02dfe77dcf44f9dda952c5d64069604042f
Your Name <you@example.com>
2018-07-10 00:45:01 +0300

    New version

--- FB_HackerCup/substr.cpp
+++ FB_HackerCup/substr.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <sstream>
 
 using namespace std;
 
@@ -38,8 +40,10 @@ string getB(string a) {
 		if (a[0] == a[i]) {
 			found = true;
 			for (int j = 1; j < a.length() - i; j ++) {
-				if (a[j] != a[i + j])
+				if (a[j] != a[i + j]) {
+					//cerr << i << " " << j;	
 					return a.substr(0, i) + a;
+				}
 			}
 		}
 	}
@@ -47,6 +51,7 @@ string getB(string a) {
 
 }
 int main() {
+	/*
 	cout << getB("ABACUS") << endl << " a " << endl;
 	cout << algo("ABACUS", "ABABACUS") << "false";
 	cout << getB("FACEBOOK") << endl << " b " << endl;
@@ -56,5 +61,44 @@ int main() {
 	cout << algo("ABACUS", "ASUCABABACUSA") << "false";
 	cout << algo("FLFE", "FLFLFE")<< "false";
 	cout << algo("XYZXZYX", "XZYXYZXYZXZYXYZXYZYX")<< "false" << endl;
+	*/
+		string s = "1\n";
+		for (int i = 0 ; i < 50 ; i ++) {
+			
+			s += "ABCD";
+		}
+			s += "ABCE";
+		s += "\n";
+		stringstream in(s);
+	int t ;
+	in >> t;
+	for (int i = 0; i < t ; i ++ ) {
+		string a;
+		in >> a;
+		cout << "Case #" << i + 1<<  ": ";
+		cout << a << endl;
+		string b = getB(a);
+		if (b  == "") {
+			cout << "Impossible" << endl; 
+			//string number = a + a;
+			/*
+			do  {//cout << "Permutation: " << number << endl;
+ 		std::size_t found = 
+				number.find(a);
+				bool b2 = algo(a , number);
+				if ( found != string::npos && !b2 )
+				{
+					cout << "Bad results " << number << " vs " <<  a << endl;
+					break;
+				}
+			}
+			while (next_permutation(number.begin(),number.end()));
+			*/
+		} else {
+			cout << b << endl;
+		}
+ 		//std::size_t found = b.find(a);
+		//cout <<  "Check " << ((b == "" && found == string::npos) || (b != "" && found != string::npos) )<< endl;
+	}
 	return 0;
 }

137092f38c00a80d522507178ee588c5eb03c140
Alexander A <gdocse@gmail.com>
2018-08-08 00:31:42 +0300

    More functions

--- CodeChef/Aug18/k_compres.cpp
+++ CodeChef/Aug18/k_compres.cpp
@@ -8,96 +8,6 @@
 using namespace std;
 
 typedef __uint128_t ll ;
-const int MOD=1000000007;
-ll ipow(ll base, ll exp)
-{
-    ll result = 1;
-    for (;;)
-    {
-        if (exp & 1)
-            result *= base;
-        exp >>= 1;
-        if (!exp)
-            break;
-        base *= base;
-    }
-
-    return result;
-}
-
-ll ipow(ll base, ll exp, ll mod)
-{
-    ll result = 1;
-    for (;;)
-    {
-        if (exp & 1) {
-            result *= base;
-            result = result % mod;
-        }
-        exp >>= 1;
-        if (!exp)
-            break;
-        base =  (base * base) % mod;
-    }
-
-    return result;
-}
-
- 
-// A function to print all prime factors of a given number n
-vector<ll> primeFactors(ll n)
-{
-    vector<ll> res;
-    // Print the number of 2s that divide n
-    while (n%2 == 0)
-    {
-        //printf("%d ", 2);
-        res.push_back(2);
-        cerr << "1 " << (long long ) 2;
-        n = n/2;
-    }
- 
-    // n must be odd at this point.  So we can skip 
-    // one element (Note i = i +2)
-    for (ll i = 3; i <= sqrt(n); i = i+2)
-    {
-        // While i divides n, print i and divide n
-        while (n%i == 0)
-        {
-            res.push_back(i);
-        cerr  << "2 " << (long long ) i;
-            n = n/i;
-        }
-    }
- 
-    // This condition is to handle the case when n 
-    // is a prime number greater than 2
-    if (n > 2) {
-        res.push_back(n);
-        cerr << "3 " <<(long long )  n;
-    }
-    return res;
-}
-
-ll gcd (ll l, ll r)
-{
-    if (l == 0 || r == 0)
-    {
-        return max(l, r);
-    }
-    ll res = 1;
-	//Find prime factors of the smaller number
-	vector<ll> pf = primeFactors(r);
-    for (ll i = 0; i < pf.size(); i++) {
-        if ( l % pf[i] == 0) {
-            l /= pf[i];
-            res *= pf[i] ;
-        }
-    }
-    return res;
-	
-	
-}
 
 std::ostream&
 operator<<( std::ostream& dest, ll value )
@@ -150,7 +60,7 @@ int algo(const vector<int> & b, int k) {
                         cerr << "++" ;
                     }
                 }
-                if (count <= b[j]) {
+                if (count == b[j]) {
                     break;
                 }
             }

0bad49aa21625665c9a821547f7fd6547c49af6d
Alexander A <gdocse@gmail.com>
2018-08-10 14:55:12 +0300

    Using permutation algorithms to get at least 10 points

--- CodeChef/Aug18/princ_dr.cpp
+++ CodeChef/Aug18/princ_dr.cpp
@@ -26,8 +26,33 @@ int main()
         for (int j = 0; j < n; j ++) {
             cin >> s[j];
         }
-        sort(s.begin(), s.end(), comp);   
+        sort(s.begin(), s.end(), comp); 
+        v = s;  
         for (int j = 0; j < n; j ++) {
+            int ma = 0;
+            do {
+                int r = 0;
+                   for (int ii = 0; ii < n; ii++) {
+                        long long sum = 0;
+                       for (int h = min(ii, j); h <= max(ii, j); h ++ ) {
+                        sum += v[h];
+                       }
+                        if (sum >= p) {
+                            r ++;
+                        }
+
+                   }
+                   if (r > ma) {
+                       ma = r;
+                   }
+
+            } while (std::next_permutation(v.begin(), v.end()));
+            if (ma == 0) {
+                cout << 0 << " ";
+            } else {
+                cout << ma << " ";
+            }
+            /*
             cerr << "Here";
             bool found = false;
             long long sum = 0;
@@ -63,7 +88,7 @@ int main()
                    if (j < n / 2) {
                    } else  {
                     cout << j - k + 1 << " ";
-                   }*/
+                   }* /
                    cout << r << " ";
                    found = true;
                    break;
@@ -73,6 +98,7 @@ int main()
             if (!found) {
                 cout << 0 << " ";
             }
+               */
         }         
         cout << endl;
     }

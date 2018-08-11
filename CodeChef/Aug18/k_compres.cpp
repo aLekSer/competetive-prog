b8e0810f81beb0fa82c45b5f7b9249f96f684020
Alexander A <gdocse@gmail.com>
2018-08-11 22:59:11 +0300

    Adding k compression with topology sorting of DAG of the dependencies < between nodes in a k neighbourhood

--- CodeChef/Aug18/k_compres.cpp
+++ CodeChef/Aug18/k_compres.cpp
@@ -34,7 +34,97 @@ operator<<( std::ostream& dest, ll value )
     }
     return dest;
 }
+// A C++ program to print topological sorting of a DAG
+#include<iostream>
+#include <list>
+#include <stack>
+using namespace std;
+ 
+// Class to represent a graph
+class Graph
+{
+    int V;    // No. of vertices'
+ 
+    // A function used by topologicalSort
+    void topologicalSortUtil(int v, bool visited[], stack<int> &Stack);
+public:
+    // Pointer to an array containing adjacency listsList
+    list<int> *adj;
+ 
+    Graph(int V);   // Constructor
+ 
+     // function to add an edge to graph
+    void addEdge(int v, int w);
+ 
+    // prints a Topological Sort of the complete graph
+    void topologicalSort(vector<int> & v);
+};
+ 
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+ 
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w); // Add w to v’s list.
+}
+ 
+// A recursive function used by topologicalSort
+void Graph::topologicalSortUtil(int v, bool visited[], 
+                                stack<int> &Stack)
+{
+    // Mark the current node as visited.
+    visited[v] = true;
+ 
+    // Recur for all the vertices adjacent to this vertex
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            topologicalSortUtil(*i, visited, Stack);
+ 
+    // Push current vertex to stack which stores result
+    Stack.push(v);
+}
+ 
+// The function to do Topological Sort. It uses recursive 
+// topologicalSortUtil()
+void Graph::topologicalSort(vector<int> & v)
+{
+    stack<int> Stack;
+ 
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+ 
+    // Call the recursive helper function to store Topological
+    // Sort starting from all vertices one by one
+    for (int i = 0; i < V; i++)
+      if (visited[i] == false)
+        topologicalSortUtil(i, visited, Stack);
+ 
+    // Print contents of stack
+    while (Stack.empty() == false)
+    {
+        cerr << Stack.top() << " ";
+        v.push_back(Stack.top());
+        Stack.pop();
+    }
+}
+int algo2(const vector<int> & b, int k, Graph &g, Graph &g2) {
+    int n = b.size();
+    for (int i = 0; i < b.size(); i++) {
+        for( int j = max(0, i -k); j < min(n, i+k); j++ ) {
+            if(b[j] < b[i]) {
+                g.addEdge(i, j);
+                g2.addEdge( j, i);
+            }
+        }
+    }
 
+}
 int algo(const vector<int> & b, int k) {
     // find 0es - set them to 1
     vector<int> c(b.size(), 1000000);
@@ -99,6 +189,48 @@ int prepare(const vector<int> & a, int s) {
     vector<int> b = a;
     const int n = a.size();
     int sum = 0;
+
+    // i is k itself
+    int k = 0;
+    for (int i = 1; i < n; i ++) {
+        Graph g(n);
+        Graph g2(n);
+        algo2(b, i, g, g2);
+        vector<int> c;
+        g.topologicalSort(c);
+        cerr << endl << i << endl;
+        vector<int> r(n);
+        for (int j = 0; j < c.size(); j++) {
+            if (g2.adj[c[j]].size() == 0) {
+                r[c[j]] = 1;
+            } else {
+                int max = 0;
+                // Set the level value of the node to the max(parents) + 1
+                for (list<int>::iterator it = g2.adj[c[j]].begin(); it != g2.adj[c[j]].end(); it++  ) {
+                    if (r[*it] > max) {
+                        max = r[*it];
+                    }
+                }
+                r[c[j]] = max + 1;
+            }
+                        
+        }
+        cerr << " Max : ";
+
+
+        int sum = 0;
+        for (int j = 0; j < c.size(); j++) {
+            cerr << r[j] << " ";
+            sum += r[j];
+
+        }
+        if (sum <= s) {
+            k = i;
+        }
+        cerr << endl;
+    }
+    cout << k + 1 << " a " << endl;
+    return 0;
     for (int j = 0; j < n; j++) {
         sum += a[j];        
     }
@@ -145,7 +277,8 @@ int prepare(const vector<int> & a, int s) {
             }
             */
         }
-        auto siz = algo(b, k);
+        //auto siz = algo(b, k);
+        /*
         if (siz <= s) {
             cerr << "Here " <<endl;
 
@@ -155,6 +288,7 @@ int prepare(const vector<int> & a, int s) {
             break;
 
         }
+        */
         
     }
     return k_m;

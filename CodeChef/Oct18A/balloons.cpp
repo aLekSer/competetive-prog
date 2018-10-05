b8499ab1b6d9531085039c3c786f1cc6eb172672
Alexander A <gdocse@gmail.com>
2018-10-06 00:23:19 +0300

    Trying to optimise and merge same iterations into one product

--- CodeChef/Oct18A/balloons.cpp
+++ CodeChef/Oct18A/balloons.cpp
@@ -36,17 +36,26 @@ int main() {
     vector<pair<ll, ll>> x;
 	for (int i = 0 ; i < N; i++)
 	{
-        x.push_back(make_pair(can(a[i], b[i], 0), i));
+        ll c = can(a[i], b[i], 0);
+        if (c > 0) {
+            x.push_back(make_pair(c, i));
+        }
     }
     make_heap(x.begin(), x.end());
-    for (int i = 0 ; i < M; i ++) {
+    for (int i = 0 ; i < M;) {
         ll mx = x.front().first;
-        mx -= b[x.front().second];
         ll id = x.front().second;
         pop_heap(x.begin(), x.end());
         x.pop_back();
+        ll mx2 = x.front().first;
+        ll num_it = min((mx - mx2) / b[id] + 1, M  - i  ) ; 
+        if (mx == mx2) {
+            num_it = 1;
+        }
+        mx -= b[x.front().second] * num_it;
         x.push_back(make_pair(mx,id));
         push_heap(x.begin(), x.end());
+        i += num_it;
 
     }
     cout << x.front().first;

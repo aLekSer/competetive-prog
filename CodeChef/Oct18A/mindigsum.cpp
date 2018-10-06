23acccbef1840cf1c8668cd215e12ef90977ed2e
Alexander A <gdocse@gmail.com>
2018-10-06 11:21:44 +0300

    Adding solution of the minimum digit sum

---
+++ CodeChef/Oct18A/mindigsum.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Command to add debuging symbols for VS Code
+// g++ -g -o ./a.out ./SHKSTR.cpp
+
+typedef long long ll;
+ll digsum(ll a) {
+    ll sum = 0;
+    while (a > 00) {
+        sum += a %10;
+        a /= 10;
+    }
+    return sum;
+}
+const ll iters = 1000; 
+void findMin(ll n, ll d, ll & mn, ll & op) {
+    mn = n;
+    op = 0;
+    for (int i = 0; i < iters; i ++) {
+        for (int j = 0; j < iters; j ++) {
+            n = digsum(n);
+            if ( n < mn ) {
+                mn = n;
+                op = i + j + 1;
+            }
+        }
+        n += d;
+    } 
+}
+
+int main() {
+	// your code goes here
+	int T;
+    cin >> T;
+	for (int i = 0 ; i < T; i++)
+	{
+	    ll n, d;
+	    cin >> n >> d;
+        ll mn = 10e10;
+        ll op = 0;
+        findMin(n , d, mn, op );
+        cout << mn << " " << op << endl;
+	}
+    return 0;
+}

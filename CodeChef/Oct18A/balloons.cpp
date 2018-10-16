2b8196988dcc4c4ea00a7bff515f03bff3ca9b1c
Alexander A <gdocse@gmail.com>
2018-10-16 20:12:23 +0300

    Removing make_pair

--- CodeChef/Oct18A/balloons.cpp
+++ CodeChef/Oct18A/balloons.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <algorithm>
 #include <math.h>
+#include <numeric>
 using namespace std;
 
 //Command to add debuging symbols for VS Code
@@ -12,27 +13,47 @@ typedef long long ll;
 ll can(ll a, ll b, ll l) {
     return max<ll> (0, (a - l)* b);
 }
+bool cmp (pair<ll, ll > a, pair<ll, ll> b) {
+    return a.first <  b.first;
+}
 int main() {
 	// your code goes here
 	ll N, M;
     cin >> N >> M;
 	vector<ll> a(N), b(N);
     map<ll, ll, greater<ll>> m;
+    ll sum = 0;
+    bool overflow = false;
 	for (int i = 0 ; i < N; i++)
 	{
 	    ll cc;
 	    cin >> cc;
 	   // a.push_back(cc);
         a[i] = cc;
+        /*
+        sum += a[i];
+        if (M < sum) {
+        // cerr << sum ;
+        overflow = true;
+        }*/
 	}
+    /*
+    if (!overflow) 
+    {
+
+            cout << 0 << endl;
+            return 0;
+    }*/
 	for (int i = 0 ; i < N; i++)
 	{
 	    ll cc;
 	    cin >> cc;
 	    //b.push_back(cc);
         b[i] = cc;
-        m.insert(make_pair(cc, i));
+        //m.insert(make_pair(cc, i));
 	}
+    //sum = std::accumulate(a.begin(), a.end(), sum);
+
     ll left = M;
     ll mx =0;
     vector<pair<ll, ll>> x(N);
@@ -40,14 +61,15 @@ int main() {
 	{
         ll c = can(a[i], b[i], 0);
         if (c > 0) {
-            x[i] = (make_pair(c, i));
+            x[i].first = c;
+            x[i].second = i;
         }
     }
-    make_heap(x.begin(), x.end());
+    make_heap(x.begin(), x.end(), cmp);
     for (int i = 0 ; i < M;) {
         ll mx = x.front().first;
         ll id = x.front().second;
-        pop_heap(x.begin(), x.end());
+        pop_heap(x.begin(), x.end(), cmp);
         x.pop_back();
         ll mx2 = x.front().first;
         ll num_it = min((mx - mx2) / b[id] + 1, M  - i  ) ; 
@@ -56,12 +78,18 @@ int main() {
             num_it = 1;
         }
         mx -= b[id] * num_it;
-        x.push_back(make_pair(mx,id));
-        push_heap(x.begin(), x.end());
+        x[x.size()-1].first = mx;
+        x[x.size()-1].second= id;
+        push_heap(x.begin(), x.end(), cmp);
         i += num_it;
 
     }
-    cout << x.front().first;
+    if (x.front().first < 0)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+    cout << x.front().first << endl;
     /*
 	for (map<ll, ll>::iterator i = m.begin(); i != m.end(); i++)
 	{
